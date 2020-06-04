50bb5b2fd03738d6ca15023c5699189e82a490aa
chikondi <chikondiman@gmail.com>
2020-06-04 15:42:35 -0400

    change welcome messagr

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">chikondiman</router-link>|
+      <router-link to="/shop">shop</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
---
+++ src/views/shop.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="products-index">
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p>
+        name:
+        <a v-bind:href="`/products/${product.id}`">{{ product.name }}</a>
+      </p>
+      <p>price: {{ product.price }}</p>
+      <img v-bind:src="product.images[1].url" />
+
+      <p>description: {{ product.description }}</p>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "come shop with chikondiman, we got 'chu",
+      products: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
